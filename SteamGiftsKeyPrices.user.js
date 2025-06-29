8f7a84e220c2c9ce0351de63bda340106b4d282b
Ivan Todorov <64197668+MapperTaurus@users.noreply.github.com>
2025-06-29 18:21:22 +0300

    Update SteamGiftsKeyPrices.user.js

--- SteamGiftsKeyPrices.user.js
+++ SteamGiftsKeyPrices.user.js
@@ -118,6 +118,9 @@
     GM_registerMenuCommand(`📄Individual View: ${individualEnabled ? 'ON' : 'OFF'}`, toggleIndividual);
     GM_registerMenuCommand(`📚List View: ${listEnabled ? 'ON' : 'OFF'}`, toggleList);
     GM_registerMenuCommand(`🔑API Key: ${apiKey ? 'SET' : 'NOT SET'}`, manageApiKey);
+    GM_registerMenuCommand(`❤ Support Me?`, () => {
+  window.open('https://github.com/MapperTaurus/SteamGifts-Key-Prices?tab=readme-ov-file#-like-this-script', '_blank');
+});
 
     // === API HELPER FUNCTIONS ===
     function buildApiUrl(endpoint, params = {}) {
